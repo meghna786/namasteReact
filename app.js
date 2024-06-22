d99e52053a150c37ea48e79d5c59c8e820d50ce0
Meghna <sharma.meghna2000@gmail.com>
2024-06-22 23:16:06 +0530

    cdn to npm package, parcel dev and prod builds, npx vs npm, ^ vs ~, type as module for scripts

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 let root = ReactDOM.createRoot(document.getElementById("root"));
 let h1 = React.createElement("h1", {
       className:'heading'
