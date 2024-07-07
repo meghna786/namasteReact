1d57fc61773e1e40a8df532da4480a0774ce00f7
Meghna <sharma.meghna2000@gmail.com>
2024-07-07 23:47:20 +0530

    updating state variables in class component and sequence of consoles of parent+ 1 child

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,15 +2,37 @@ import React from 'react';
 import User from './User';
 import UserClass from './UserClass';
 
-const About = () => {
-  return (
-    <div>
+
+class About extends React.Component{
+  constructor(props){
+    super(props);
+    console.log('parent constructor');
+  }
+  componentDidMount(){
+    console.log('parent componentDidMount');
+  }
+
+  render(){
+    console.log('parent render');
+    return (
+      <div>
       Abpout us page
       <User name={'Meghna Sharma'} description={'aint i your kkkg pooh in tech'} location={'wont disclose'} />
       <UserClass name={'Meghna Sharma'} description={'need to learn so much more, and have to have to acheive a lot more'} 
       location={'in my head'} />
     </div>
-  )
+    )
+  }
 }
+// const About = () => {
+//   return (
+//     <div>
+//       Abpout us page
+//       <User name={'Meghna Sharma'} description={'aint i your kkkg pooh in tech'} location={'wont disclose'} />
+//       <UserClass name={'Meghna Sharma'} description={'need to learn so much more, and have to have to acheive a lot more'} 
+//       location={'in my head'} />
+//     </div>
+//   )
+// }
 
 export default About;
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -2,15 +2,32 @@ import React from 'react';
 
 class UserClass extends React.Component{
       constructor(props){
-            super(props);
+            super(props); // its parent class is not About, but React.Component
+            this.state={
+                  count:0,
+                  count2:2
+            }
+            console.log('child constructor');
+      }
+
+      componentDidMount(){
+            console.log('child componentDidMount');
       }
 
       render(){
+            console.log('child render');
             return (
                   <div style={{border:'1px solid black'}}>
                   <h1>Name- {this.props.name}</h1>
                   <h3>Location- {this.props.location}</h3>
                   <h3>Description- {this.props.description}</h3>
+                  <h3>count- {this.state.count}</h3>
+                  <button onClick={()=>{
+                        this.setState({
+                              count:this.state.count+1
+                        })
+                  }}>Increase count value</button>
+                  <h3>count2- {this.state.count2}</h3>
             </div>
             )
       }
